adab2fbe5c831d189c68a1a2a0d7b82e0453344c
Sébastien - fisher01 <contact@sebastienforay.fr>
2020-10-24 23:29:15 +0200

    User profile UI refreshed

---
+++ Rocks.Wasabee.Mobile.Core.Ui/Helpers/Converters/AgentVerificationBoolToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Rocks.Wasabee.Mobile.Core.Ui.Helpers.Converters
+{
+    public class AgentVerificationBoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "Verified" : "Unverified";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
