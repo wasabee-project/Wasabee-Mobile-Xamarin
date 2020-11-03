e69ba9075481ed96d9b64bc2ce396483853749d0
Sébastien - fisher01 <contact@sebastienforay.fr>
2020-11-03 18:24:38 +0100

    Added link to privacy policy in loc sharing warning dialog

--- Rocks.Wasabee.Mobile.Core/ViewModels/Dialogs/LocationWarningDialogViewModel.cs
+++ Rocks.Wasabee.Mobile.Core/ViewModels/Dialogs/LocationWarningDialogViewModel.cs
@@ -2,6 +2,10 @@
 using MvvmCross.Plugin.Messenger;
 using Rocks.Wasabee.Mobile.Core.Messages;
 using Rocks.Wasabee.Mobile.Core.Services;
+using Rocks.Wasabee.Mobile.Core.Settings.Application;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Essentials.Interfaces;
 
 namespace Rocks.Wasabee.Mobile.Core.ViewModels.Dialogs
@@ -10,12 +14,14 @@ namespace Rocks.Wasabee.Mobile.Core.ViewModels.Dialogs
     {
         private readonly IMvxMessenger _messenger;
         private readonly IPreferences _preferences;
+        private readonly IAppSettings _appSettings;
 
         public LocationWarningDialogViewModel(IDialogNavigationService dialogNavigationService, IMvxMessenger messenger,
-            IPreferences preferences) : base(dialogNavigationService)
+            IPreferences preferences, IAppSettings appSettings) : base(dialogNavigationService)
         {
             _messenger = messenger;
             _preferences = preferences;
+            _appSettings = appSettings;
         }
 
         public override void ViewAppeared()
@@ -40,6 +46,12 @@ namespace Rocks.Wasabee.Mobile.Core.ViewModels.Dialogs
             _messenger.Publish(new MessageFrom<LocationWarningDialogViewModel>(this, NeverShowAgain));
 
             base.CloseCommand.Execute();
+        }
+        public IMvxAsyncCommand OpenPrivacyPolicyCommand => new MvxAsyncCommand(OpenPrivacyPolicyExecuted);
+        private async Task OpenPrivacyPolicyExecuted()
+        {
+            var uri = _appSettings.WasabeeBaseUrl + "/privacy";
+            await Launcher.OpenAsync(new Uri(uri));
         }
 
         #endregion
