5cbc7c3732cae4745c84544ec578fc2bc118b3d2
Sébastien - fisher01 <contact@sebastienforay.fr>
2020-11-11 19:55:45 +0100

    Improved DialogNavigationService to use navigation results
    + Added try catch to prevent app crash when cancelling permission request

--- Rocks.Wasabee.Mobile.Core/Services/IDialogNavigationService.cs
+++ Rocks.Wasabee.Mobile.Core/Services/IDialogNavigationService.cs
@@ -6,8 +6,13 @@ namespace Rocks.Wasabee.Mobile.Core.Services
     public interface IDialogNavigationService
     {
         Task Navigate<TViewModel>() where TViewModel : IMvxViewModel;
-        Task Navigate<TViewModel, TParameter>(TParameter param) where TViewModel : IMvxViewModel<TParameter> where TParameter : class;
+        Task Navigate<TViewModel, TParameter>(TParameter param) where TViewModel : IMvxViewModel<TParameter>
+            where TParameter : notnull;
+        Task<TResult> Navigate<TViewModel, TResult>() where TViewModel : IMvxViewModelResult<TResult>
+            where TResult : notnull;
 
-        Task Close(bool animated = true);
+        Task<bool> Close(bool animated = true);
+        Task<bool> Close<TResult>(IMvxViewModelResult<TResult> viewModel, bool animated = true, TResult closeResult = default)
+            where TResult : notnull;
     }
 }
