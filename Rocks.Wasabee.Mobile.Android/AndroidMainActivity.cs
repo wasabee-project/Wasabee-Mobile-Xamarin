e6d253ded704a5893a614bf2ce562285437479be
Sébastien - fisher01 <contact@sebastienforay.fr>
2020-12-05 18:05:53 +0100

    Force splashscreen portrait

--- Rocks.Wasabee.Mobile.Android/AndroidMainActivity.cs
+++ Rocks.Wasabee.Mobile.Android/AndroidMainActivity.cs
@@ -24,6 +24,7 @@ using System;
 using System.Collections.Generic;
 using Xamarin.Forms.GoogleMaps.Android;
 using Action = Rocks.Wasabee.Mobile.Core.Messages.Action;
+using Orientation = Rocks.Wasabee.Mobile.Core.Messages.Orientation;
 
 namespace Rocks.Wasabee.Mobile.Droid
 {
@@ -38,6 +39,7 @@ namespace Rocks.Wasabee.Mobile.Droid
 
         private MvxSubscriptionToken _token;
         private MvxSubscriptionToken _tokenTheme;
+        private MvxSubscriptionToken _tokenOrientation;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -96,6 +98,15 @@ namespace Rocks.Wasabee.Mobile.Droid
                 });
 
                 _tokenTheme = Mvx.IoCProvider.Resolve<IMvxMessenger>().Subscribe<ChangeThemeMessage>(msg => OnThemeChanged(msg.Theme));
+                _tokenOrientation = Mvx.IoCProvider.Resolve<IMvxMessenger>().Subscribe<ChangeOrientationMessage>(msg =>
+                {
+                    if (msg.Orientation == Orientation.Portait)
+                        RequestedOrientation = ScreenOrientation.Portrait;
+                    else if (msg.Orientation == Orientation.Landscape)
+                        RequestedOrientation = ScreenOrientation.Landscape;
+                    else if (msg.Orientation == Orientation.Any)
+                        RequestedOrientation = ScreenOrientation.Unspecified;
+                });
 
 #if DEBUG
                 if (System.Diagnostics.Debugger.IsAttached)
