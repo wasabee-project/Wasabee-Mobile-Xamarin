0a49ea15e3295f034c5a9e0c844d31c4baed5a76
Sebastien - fisher01 <contact@sebastienforay.fr>
2021-03-22 23:19:13 +0100

    WIP : Operation checklist

---
+++ Rocks.Wasabee.Mobile.Core.Ui/Views/Operation/ChecklistPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.Forms.Presenters.Attributes;
+using Rocks.Wasabee.Mobile.Core.ViewModels.Operation;
+using Xamarin.Forms.Xaml;
+
+namespace Rocks.Wasabee.Mobile.Core.Ui.Views.Operation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [MvxTabbedPagePresentation(Position = TabbedPosition.Tab, NoHistory = true, Icon = "checklist.png")]
+    public partial class ChecklistPage : BaseContentPage<ChecklistViewModel>
+    {
+        public ChecklistPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Rocks.Wasabee.Mobile.Core/ViewModels/Operation/ChecklistViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+
+namespace Rocks.Wasabee.Mobile.Core.ViewModels.Operation
+{
+    public class ChecklistViewModel : BaseViewModel
+    {
+        public ChecklistViewModel()
+        {
+            
+        }
+
+        public override Task Initialize()
+        {
+            return base.Initialize();
+        }
+
+        #region Properties
+
+        
+
+        #endregion
+
+        #region Command
+
+        
+
+        #endregion
+
+        #region Private methods
+
+
+
+        #endregion
+    }
+}
