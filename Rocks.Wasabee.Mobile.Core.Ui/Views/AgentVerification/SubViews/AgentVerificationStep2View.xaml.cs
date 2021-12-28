389342d212dbadfdad1e0b9494fc439c693bb8c7
Sebastien - fisher01 <contact@sebastienforay.fr>
2021-12-28 00:37:04 -0500

    ACV step 2 : Secured agent name input

--- Rocks.Wasabee.Mobile.Core.Ui/Views/AgentVerification/SubViews/AgentVerificationStep2View.xaml.cs
+++ Rocks.Wasabee.Mobile.Core.Ui/Views/AgentVerification/SubViews/AgentVerificationStep2View.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Rocks.Wasabee.Mobile.Core.ViewModels.AgentVerification.SubViewModels;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Text.RegularExpressions;
 
 namespace Rocks.Wasabee.Mobile.Core.Ui.Views.AgentVerification.SubViews
 {
@@ -9,6 +11,23 @@ namespace Rocks.Wasabee.Mobile.Core.Ui.Views.AgentVerification.SubViews
         public AgentVerificationStep2View()
         {
             InitializeComponent();
+
+            AgentNameEntry.EntryField.TextChanged += EntryField_TextChanged;
+        }
+
+        private void EntryField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is Entry entry)
+            {
+                var text = e.NewTextValue;
+                if (text.Length > 16)
+                    text = text.Substring(0, 16);
+
+                var regex = new Regex("[a-zA-Z0-9]{0,16}");
+                var match = regex.Match(text);
+                if (match.Success)
+                    entry.Text = match.Value;
+            }
         }
     }
 }
