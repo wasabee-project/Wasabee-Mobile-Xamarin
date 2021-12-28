7872216d0f4470fac5171fda75de4c4f14bfc927
Sebastien - fisher01 <contact@sebastienforay.fr>
2021-12-27 23:58:10 -0500

    iOS : Added missing custom renderer + fixed some UI details

---
+++ Rocks.Wasabee.Mobile.iOS/Renderers/CustomButtonRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Rocks.Wasabee.Mobile.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Button), typeof(CustomButtonRenderer))]
+namespace Rocks.Wasabee.Mobile.iOS.Renderers
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element != null)
+            {
+                Element.Padding = new Thickness(10, 5);
+                Element.Text = Element.Text.ToUpperInvariant();
+                Element.FontAttributes = FontAttributes.Bold;
+            }
+        }
+    }
+}
