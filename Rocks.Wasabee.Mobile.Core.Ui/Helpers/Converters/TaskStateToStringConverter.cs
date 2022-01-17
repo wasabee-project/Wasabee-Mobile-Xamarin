f031f0452742a18de51ed58fa552d3a1679a7b98
Sebastien - fisher01 <contact@sebastienforay.fr>
2022-01-17 18:09:00 -0500

    Added missing valueconverter for TaskState to translate markers / links dialogs state

---
+++ Rocks.Wasabee.Mobile.Core.Ui/Helpers/Converters/TaskStateToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using Rocks.Wasabee.Mobile.Core.Helpers;
+using Rocks.Wasabee.Mobile.Core.Models.Operations;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Rocks.Wasabee.Mobile.Core.Ui.Helpers.Converters
+{
+    public class TaskStateToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TaskState state)
+            {
+                return state.ToFriendlyString();
+            }
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
