774e59d20ed0a16e43efd4a901df980a14bd1ee4
Sébastien - fisher01 <contact@sebastienforay.fr>
2023-11-05 16:16:43 -0500

    fix webauthcallback

--- Rocks.Wasabee.Mobile.Android/WebAuthenticationCallbackActivity.cs
+++ Rocks.Wasabee.Mobile.Android/WebAuthenticationCallbackActivity.cs
@@ -5,7 +5,7 @@ using Xamarin.Essentials;
 
 namespace Rocks.Wasabee.Mobile.Droid
 {
-    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = false)]
+    [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
     [IntentFilter(new[] { Intent.ActionView},
         Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable},
         DataScheme = "com.googleusercontent.apps.269534461245-ltpks4ofjh9epvida0ct965829i4cfsi" )]
